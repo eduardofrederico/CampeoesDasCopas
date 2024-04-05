f503816725c596ccdd4e69788dac91c46631a375
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-04-05 20:30:32 -0300

    Preenchendo tabelas.

--- CampeoesDasCopas/Controllers/WinnersTableViewController.swift
+++ CampeoesDasCopas/Controllers/WinnersTableViewController.swift
@@ -48,8 +48,11 @@ class WinnersTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
+        let worldCup = worldCups[indexPath.row]
+        cell.textLabel?.text = "Copa \(worldCup.year) - \(worldCup.country)"
+        cell.detailTextLabel?.text = "Copa \(worldCup.winner) - \(worldCup.vice)"
+        cell.imageView?.image = UIImage(named: "\(worldCup.winner).png")
         
-
         return cell
     }
     
