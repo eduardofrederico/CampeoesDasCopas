9af581f0513bb246ca42ce0e945f1c95d4afebde
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-04-20 00:12:17 -0300

    Trabalhando com TableView em ViewControllers.

--- CampeoesDasCopas/Controllers/WorldCupViewController.swift
+++ CampeoesDasCopas/Controllers/WorldCupViewController.swift
@@ -17,7 +17,7 @@ class WorldCupViewController: UIViewController {
     @IBOutlet weak var lbScore: UILabel!
     @IBOutlet weak var lbWinner: UILabel!
     @IBOutlet weak var lbVice: UILabel!
-   
+    @IBOutlet weak var tableview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,16 +30,27 @@ class WorldCupViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+
+}
+
+extension WorldCupViewController: UITableViewDataSource {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return worldCup.matches.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let games = worldCup.matches[section].games
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+}
 
+extension WorldCupViewController: UITableViewDelegate {
+    
 }
